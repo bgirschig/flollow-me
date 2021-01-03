af4ec81321643b6a83ffcea928bed5cdd7ce0c88
bastien girschig <bastien.girschig@gmail.com>
2021-01-03 19:40:07 +0100

    working rolling array

--- RollingArray.cpp
+++ RollingArray.cpp
@@ -1,18 +1,43 @@
-#include "Arduino.h"
 #include "RollingArray.h"
 
-RollingArray::RollingArray(int size) {
-  max_size = size;
-  current_size = 0;
-  currentIndex = 0;
+RollingArray::RollingArray(int alloc_size) {
+  this->alloc_size = alloc_size;
+  values = new float[alloc_size];
+
+  for (int i = 0; i < alloc_size; i++) values[i] = 0;
+  setLength(alloc_size);
+}
+
+void RollingArray::setLength(int size) {
+  this->size = size;
+  start_idx = 0;
   sum = 0;
-  values = new float[size];
+  
+  // reset array values
+  for (int i = 0; i < size; i++) values[i] = 0;
+}
+
+float RollingArray::get(int index) {
+  return values[(start_idx+index) % size];
 }
 
 void RollingArray::append(float value) {
-  values[currentIndex] = value;
-  currentIndex += 1;
-  sum += value;
-  if (current_size < max_size) current_size += 1;
-  if (currentIndex >= max_size) currentIndex = 0;
+  start_idx += 1;  
+  if(start_idx >= size) start_idx = 0;
+
+  int target_idx = (start_idx + size - 1) % size;
+  sum = sum + value - values[target_idx];
+  values[target_idx] = value;
+}
+
+std::string RollingArray::toString() {
+  std::stringstream output;
+
+  output << "array(size:" << size << " sum:" << sum << ")";
+  
+  output << " [";
+  for (int i = 0; i < size; i++) output << get(i) << ",";
+  output << "]\n";
+
+  return output.str();
 }
